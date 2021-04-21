407c5a87a4f8278a7917060183e227bb992c8693
viktoriia-abalymova <72358498+viktoriia-abalymova@users.noreply.github.com>
2021-04-21 11:52:08 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,36 +1,41 @@
 import os
 import pandas as pd
-from airflow.models import DAG, Variable
-
-def get_path(file_name):
-    return os.path.join(os.path.expanduser('~'), file_name)
+from sqlalchemy import create_engine
+from airflow.hooks.base import BaseHook
+from airflow.models import Variable
 
 
 def download_titanic_dataset(**context):
     url = 'https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
     df = pd.read_csv(url)
-    #df.to_csv(get_path('titanic.csv'), encoding='utf-8')
     dt = df.to_json()
     context['task_instance'].xcom_push(key='download_titanic_dataset', value=dt)
 
 
 def pivot_dataset(**context):
-    titanic_df = pd.read_csv(get_path('titanic.csv'))
-    df = titanic_df.pivot_table(index=['Sex'],
-                                columns=['Pclass'],
-                                values='Name',
-                                aggfunc='count').reset_index()
-    #df.to_csv(get_path('titanic_pivot.csv'))
-    dt = df.to_json()
-    context['task_instance'].xcom_push(key='pivot_dataset', value=dt)
-
+    titanic_df = context['task_instance'].xcom_pull(key='download_titanic_dataset', task_ids='download_titanic_dataset')
+    dt = pd.read_json(titanic_df)
+    df = dt.pivot_table(index=['Sex'],
+                        columns=['Pclass'],
+                        values='Name',
+                        aggfunc='count').reset_index()
+    push_to_postgresql('pivot_titanic', df)
 
 def mean_fare_per_class(**context):
-    tt_df = pd.read_csv(get_path('titanic.csv'))
-    df = tt_df.groupby('Pclass')['Fare'].mean()
-    #df.to_csv(get_path('titanic_mean_fares.csv'))
-    dt = df.to_json()
-    context['task_instance'].xcom_push(key='mean_fare_per_class', value=dt)
+    titanic_df = context['task_instance'].xcom_pull(key='download_titanic_dataset', task_ids='download_titanic_dataset')
+    df = pd.read_json(titanic_df)
+    dt = df.groupby('Pclass')['Fare'].mean()
+    push_to_postgresql('mean_fare_titanic', dt)
 
 
+#@python_operator()
+def push_to_postgresql(db_name, dataframe):
+   # create sql engine for sqlalchemy
+   alchemyEngine = create_engine('postgresql://admin:memide05@localhost:5432/gb_airflow')
+   postgreSQLConnection = alchemyEngine.connect()
+   postgreSQLTable = db_name
+   dataframe.to_sql(postgreSQLTable, postgreSQLConnection, if_exists='replace')
+   postgreSQLConnection.close()
+   pivot = Variable.get('pivot')
+   mean_fare = Variable.get('mean_fares')
 
